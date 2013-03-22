b76f7e6a78c5b1cced13d951f193de7b5c778f22
r2mzes <r2mzes@gmail.com>
2013-03-22 13:26:17 +0100

    Glowna czesc programu do main()

--- converter.py
+++ converter.py
@@ -1,15 +1,15 @@
 from font import Font, FontLoader
 
+def main():
+    text = getText()
+    font = FontLoader().loadFont('fancyFont/')
+
+    drawer = TextDrawer()
+    drawer.setFont(font)
+    drawer.draw(text)
+
 def getText():
     return 'Git'
-
-class Font:
-    def __init__(self):
-        self.letters = {}
-
-class FontLoader:
-    def loadFont(self, directory):
-        pass
 
 class TextDrawer:
     def setFont(self, font):
@@ -18,9 +18,5 @@ class TextDrawer:
     def draw(self, text):
         pass
 
-text = getText()
-font = FontLoader().loadFont('fancyFont/')
-
-drawer = TextDrawer()
-drawer.setFont(drawer)
-font.draw(text)
+if __name__ == '__main__':
+    main()
