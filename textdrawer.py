3df42448a70eeef7321f8fc2e75feecb7342f895
r2mzes <r2mzes@gmail.com>
2013-03-22 18:55:23 +0100

    poprawiony FontLoader

--- textdrawer.py
+++ textdrawer.py
@@ -6,15 +6,3 @@ class TextDrawer:
         for character in text:
             print self.font.letters[character.upper()]
 			
-			
-class FontLoader:
-    def loadFont(self, directory):
-        font = Font("fancy")
-
-        for filename in os.listdir(directory):
-            font.letters[filename] = readFile(directory+filename)
-
-        return font
-
-def readFile(filename):
-    return open(filename).read()
