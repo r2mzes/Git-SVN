d5abbef9be3b69fc61734edac807dd1a4f7c0b9e
antoni <antkiewicz.mateusz@gmail.com>
2013-03-22 13:09:26 +0100

    mock klasy konwertujacej

--- converter.py
+++ converter.py
@@ -7,6 +7,13 @@ class Font:
 class FontLoader:
     def loadFont(self, directory):
         pass
+		
+class TextDrawer:
+    def setFont(self, font):
+        pass
+
+    def draw(self, text):
+        pass
 
 t = getText()
 f = FontLoader().loadFont('fancyFont/')
