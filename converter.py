4d0354323d4a2af5807d3f4425440ea8375e0811
antoni <antkiewicz.mateusz@gmail.com>
2013-03-22 19:24:59 +0100

    Revert Seba

--- converter.py
+++ converter.py
@@ -10,12 +10,8 @@ def main():
     drawer.setFont(font)
     drawer.draw(text)
 
- def getText():
-    if len(sys.argv) != 1:
-        print "Bad arguments"
-        sys.exit(1)
-
-    return sys.argv[0]
+def getText():
+	return sys.argv[0]
 
 if __name__ == '__main__':
     main()
