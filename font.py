1a84900526e8f97e45499af18d28c69402217313
r2mzes <r2mzes@gmail.com>
2013-03-22 13:34:30 +0100

    czcionka dla I

--- font.py
+++ font.py
@@ -4,15 +4,22 @@ class Font:
 class FontLoader:
     def loadFont(self, directory):
         font = Font("fancy")
-		
         font.letters['G'] = r'''
   ___ 
  / __)
 ( (_ \
  \___/
 '''
-		
-	
+
+		font.letters['I'] = r'''
+  __  
+ (  ) 
+  )(  
+ (__) 
+'''		
+return font
+
+
 class Font:
     def __init__(self, name):
         self.name = name
